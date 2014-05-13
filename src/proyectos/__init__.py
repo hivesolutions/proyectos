ff04d74780f5c0f617b9169ec4edf3d662f4c5d4
joamag <joamag@gmail.com>
2014-05-13 12:33:48 +0100

    new models import

--- src/proyectos/__init__.py
+++ src/proyectos/__init__.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from . import controllers
+from . import models
 from . import util
 from . import main
 
 from .controllers import *
+from .models import *
 from .util import *
 from .main import *
