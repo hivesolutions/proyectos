6a2ed9e03546d63a81e119ab4e53dcededfaa7a8
joamag <joamag@gmail.com>
2014-05-13 16:42:19 +0100

    initial scheduling start

--- src/proyectos/main.py
+++ src/proyectos/main.py
@@ -4,6 +4,8 @@
 import appier
 import appier_extras
 
+from proyectos import scheduler
+
 class ProyectosApp(appier.WebApp):
 
     def __init__(self):
@@ -14,6 +16,11 @@ class ProyectosApp(appier.WebApp):
                 appier_extras.AdminPart,
             )
         )
+        self.scheduler = scheduler.Scheduler(self)
+
+    def start(self):
+        appier.WebApp.start(self)
+        self.scheduler.start()
 
 if __name__ == "__main__":
     app = ProyectosApp()
