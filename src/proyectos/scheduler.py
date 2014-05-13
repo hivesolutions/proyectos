2c83d862eb32b43d5e77fcc7089529ef39276662
joamag <joamag@gmail.com>
2014-05-13 16:50:20 +0100

    new timeout

--- src/proyectos/scheduler.py
+++ src/proyectos/scheduler.py
@@ -9,7 +9,7 @@ import appier
 import github
 import proyectos
 
-LOOP_TIMEOUT = 60.0
+LOOP_TIMEOUT = 1800.0
 """ The time value to be used to sleep the main sequence
 loop between ticks, this value should not be too small
 to spend many resources or to high to create a long set
