2b6fca842e2957a38c8ce70162897682966ed105
joamag <joamag@gmail.com>
2014-05-13 12:34:15 +0100

    new ensure support

--- src/proyectos/util/logic.py
+++ src/proyectos/util/logic.py
@@ -5,6 +5,8 @@ import appier
 
 import github
 
+import proyectos
+
 def get_api():
     api = github.Api(
         username = appier.conf("GITHUB_USERNAME"),
@@ -12,11 +14,33 @@ def get_api():
     )
     return api
 
-def all_repos():
+def all_repos(ensure = True):
     api = get_api()
     all_repos = api.self_repos()
     self_orgs = api.self_orgs()
     for org in self_orgs:
         org_repos = api.repos_org(org["login"])
         all_repos.extend(org_repos)
+    if ensure: return ensure_repos(all_repos)
     return all_repos
+
+def ensure_repos(repos):
+    _repos = []
+    for repo in repos:
+        _repo = ensure_repo(repo)
+        _repos.append(_repo)
+    return _repos
+
+def ensure_repo(repo):
+    _repo = proyectos.Repo.get(
+        full_name = repo["full_name"],
+        raise_e = False
+    )
+    if _repo: return _repo
+    _repo = proyectos.Repo(
+        name = repo["name"],
+        full_name = repo["full_name"],
+        status = False
+    )
+    _repo.save()
+    return _repo
