9d279644a608d91a7a2e6d68d75c5144f0d21a28
joamag <joamag@gmail.com>
2014-05-14 10:59:49 +0100

    better proyectos scheduler

--- src/proyectos/scheduler.py
+++ src/proyectos/scheduler.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import time
-import threading
-import traceback
-
 import appier
 import github
 import proyectos
@@ -15,37 +11,18 @@ loop between ticks, this value should not be too small
 to spend many resources or to high to create a long set
 of time between external interactions """
 
-class Scheduler(threading.Thread):
-    """
-    Scheduler class that handles all the async tasks
-    relates with the house keeping of the proyectos
-    infra-structure. The architecture of the logic
-    for the class should be modular in the sense that
-    new task may be added to it through a queue system.
-    """
+class Scheduler(appier.Scheduler):
 
-    def __init__(self, owner):
-        threading.Thread.__init__(self)
-        self.owner = owner
-        self.daemon = True
-
-    def run(self):
-        self.running  = True
-        self.load()
-        while self.running:
-            try:
-                self.tick()
-            except BaseException as exception:
-                self.owner.logger.critical("Unhandled proyectos exception raised")
-                self.owner.logger.error(exception)
-                lines = traceback.format_exc().splitlines()
-                for line in lines: self.owner.logger.warning(line)
-            time.sleep(LOOP_TIMEOUT)
-
-    def stop(self):
-        self.running = False
+    def __init__(self, *args, **kwargs):
+        appier.Scheduler.__init__(
+            self,
+            timeout = LOOP_TIMEOUT,
+            *args,
+            **kwargs
+        )
 
     def tick(self):
+        appier.Scheduler.tick(self)
         self.check_repos()
         self.update_repos()
 
@@ -59,9 +36,9 @@ class Scheduler(threading.Thread):
         )
 
     def check_repos(self):
-        self.owner.logger.debug("Checking github repos ...")
+        self.logger.debug("Checking github repos ...")
         proyectos.sync_repos(api = self.github, ensure = True)
 
     def update_repos(self):
-        self.owner.logger.debug("Updating github repos ...")
+        self.logger.debug("Updating github repos ...")
         proyectos.update_repos()
