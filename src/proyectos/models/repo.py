3114d8e3d1db14a057045d23f1518660e150422b
joamag <joamag@gmail.com>
2014-05-14 11:12:03 +0100

    new process structure

--- src/proyectos/models/repo.py
+++ src/proyectos/models/repo.py
@@ -83,8 +83,13 @@ class Repo(appier_extras.admin.Base):
             (self.full_name, repo_path)
         )
 
-        if is_new: subprocess.call(["git", "clone", self.clone_url, repo_path])
-        else: subprocess.call(["git", "-C", repo_path, "pull"])
+        if is_new: popen = subprocess.Popen(["git", "clone", self.clone_url, repo_path])
+        else: popen = subprocess.Popen(["git", "pull"], cwd = repo_path)
+
+        result = popen.wait()
+        if result == 0: return
+
+        raise appier.OperationalError(message = "Problem executing command")
 
     def repo_path(self):
         repos_path = appier.conf("REPOS_PATH", "repos")
