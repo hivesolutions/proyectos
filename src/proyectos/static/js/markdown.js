22d8fa1bbae37eee8b97843d2b3efe77f4c6b3b6
joamag <joamag@gmail.com>
2015-09-26 18:59:42 +0100

    better post applied event

--- src/proyectos/static/js/markdown.js
+++ src/proyectos/static/js/markdown.js
@@ -32,6 +32,18 @@
 jQuery(document).ready(function() {
             var _body = jQuery("body");
             _body.bind("applied", function(event, base) {
+                        // runs the initial apply operation so that the elements
+                        // are registered for the domain specific operations
                         base.uapply();
+                    });
+            _body.bind("post_applied", function(event, base) {
+                        // verifies if this is a global selection and if that's
+                        // the case refreshes the location of the document so
+                        // that any hash based scroll operation is now performed
+                        // with the "new" look and feel (as expected)
+                        var isBody = base.is("body");
+                        isBody && setTimeout(function() {
+                                    document.location = document.location;
+                                });
                     });
         });
